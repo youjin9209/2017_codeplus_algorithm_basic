ecf48eb5ab792b01be1cb8a55a4562c81f86ce29
한유진 <scarlett@han-yujin-ui-MacBook-Air.local>
2017-01-28 18:30:34 +0900

    A+B

---
+++ bj_10950.java
@@ -0,0 +1,17 @@
+package algorithm_basic;
+import java.util.*;
+
+public class bj_10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		sc.nextLine();
+		while(T-- > 0) {
+			int a, b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
